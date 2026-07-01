08b171c0ba2f2083d251286a1989cf8e9001d98d
Paula Beatriz <paula.beatriz@upe.br>
2026-07-01 14:25:23 -0300

    feat: player consegue matar inimigo estagram

--- dont-scroll/entities/enemies/enemy.gd
+++ dont-scroll/entities/enemies/enemy.gd
@@ -1,29 +1,47 @@
 extends CharacterBody2D
 
-
 @export var speed := 40.0
 @export var gravity := 900.0
 
-
 var direction := 1
+var dead := false
 
+@onready var sprite = $AnimatedSprite2D
 
-@onready var floor_check = $FloorCheck
-@onready var wall_check = $WallCheck
-
+func _ready():
+	add_to_group("enemies")
 
 func _physics_process(delta):
+	if dead:
+		return
 
 	velocity.x = 0
-	velocity.y = 0
+
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
 	move_and_slide()
 
-
 func change_direction():
-
 	direction *= -1
+	sprite.flip_h = direction < 0
 
+func take_damage():
 
-	if has_node("AnimatedSprite2D"):
-		$AnimatedSprite2D.flip_h = direction < 0
+	if dead:
+		return
+
+	dead = true
+
+	velocity = Vector2.ZERO
+
+	await blink_animation()
+
+	queue_free()
+	
+func blink_animation():
+	for i in range(6):
+		sprite.visible = false
+		await get_tree().create_timer(0.1).timeout
+		sprite.visible = true
+		await get_tree().create_timer(0.1).timeout
